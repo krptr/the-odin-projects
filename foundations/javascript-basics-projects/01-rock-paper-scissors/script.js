6bc32831816645afa349cbbb0c0b037e0b7187be
ktogre <212162456+ktogre@users.noreply.github.com>
2026-02-02 18:44:43 +0100

    Add RPS UI features

--- foundations/javascript-basics-projects/01-rock-paper-scissors/script.js
+++ foundations/javascript-basics-projects/01-rock-paper-scissors/script.js
@@ -22,40 +22,75 @@ function getComputerChoice(rock, paper, scissors, min, max) {
 }
 
 function getHumanChoice(humanChoice) {
-    humanChoice = prompt("Enter your choice", '');
     return humanChoice.toLowerCase();
 }
 
 function playGame() {
+    const body = document.querySelector("body");
+    const div = document.createElement("div");
+    div.setAttribute("style", "background-color: white")
+    // div.setAttribute("style", "border: 1px solid black; background-color: pink");
+
     let humanScore = 0;
     let computerScore = 0;
 
     function playRound(humanChoice, computerChoice) {
         if (humanChoice === computerChoice) {
-            console.log("It's a draw! Play again");
+            const draw = document.createElement("p");
+            draw.setAttribute("style", "width: 200px; color: grey; font-weight: bold");
+            draw.textContent = "It's a draw! Play Again";
+            div.appendChild(draw);
             return;
         } else if ((humanChoice === "rock" && computerChoice == "scissors") ||
             (humanChoice === "scissors" && computerChoice == "paper") ||
             (humanChoice === "paper" && computerChoice == "rock")) {
-            console.log(`You won! ${humanChoice} beats ${computerChoice}.`);
+            const humanWins = document.createElement("p");
+            humanWins.setAttribute("style", "width: 200px; color: green; font-weight: bold");
+            humanWins.textContent = `You won! ${humanChoice} beats ${computerChoice}.`;
+            div.appendChild(humanWins);
             humanScore += 1;
         } else if ((computerChoice === "rock" && humanChoice == "scissors") ||
             (computerChoice === "scissors" && humanChoice == "paper") ||
             (computerChoice === "paper" && humanChoice == "rock")) {
-            console.log("Computer wins! Rock beats Scissors.");
+            const computerWins = document.createElement("p");
+            computerWins.setAttribute("style", "width: 200px; color: red; font-weight: bold");
+            computerWins.textContent = `Computer won! ${computerChoice} beats ${humanChoice}.`;
+            div.appendChild(computerWins);
             computerScore += 1;
         } else {
-            console.log("Invalid choice! Try again");
+            const invalidChoice = document.createElement("p");
+            invalidChoice.textContent = "Invalid choice! Try again";
+            div.appendChild(invalidChoice);
         }
     }
 
-    for (let i = 1; i <= 5; i++) {
-        const humanSelection = getHumanChoice();
+    function processChoice(choice) {
+        const humanSelection = getHumanChoice(choice);
         const computerSelection = getComputerChoice();
         playRound(humanSelection, computerSelection);
-        console.log(`Your Score: ${humanScore}
-        Computer Score: ${computerScore}`);
     }
+
+    const btn1 = document.querySelector("#btn1");
+    const btn2 = document.querySelector("#btn2");
+    const btn3 = document.querySelector("#btn3");
+
+    btn1.addEventListener("click", () => {
+        processChoice("rock");
+        body.appendChild(div);
+    });
+
+    btn2.addEventListener("click", () => {
+        processChoice("paper");
+        body.appendChild(div);
+    });
+
+    btn3.addEventListener("click", () => {
+        processChoice("scissors");
+        body.appendChild(div);
+    });
 }
+
+
+
 
 playGame();
