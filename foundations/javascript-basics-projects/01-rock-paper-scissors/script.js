545752bc38c676f56f8a173907a22a9a0427761a
ktogre <212162456+ktogre@users.noreply.github.com>
2026-02-06 09:15:47 +0100

    Add the UI for the rock, paper, scissors game

--- foundations/javascript-basics-projects/01-rock-paper-scissors/script.js
+++ foundations/javascript-basics-projects/01-rock-paper-scissors/script.js
@@ -1,5 +1,13 @@
 let humanScore = 0;
 let computerScore = 0;
+let gameState = true;
+const humanScoreTracker = document.querySelector("#human-score");
+const computerScoreTracker = document.querySelector("#computer-score");
+const body = document.querySelector("body");
+
+const div = document.createElement("div");
+div.setAttribute("style", "background-color: white");
+
 
 function getComputerChoice(rock, paper, scissors, min, max) {
     rock = "rock";
@@ -26,22 +34,16 @@ function getHumanChoice(humanChoice) {
 }
 
 function playGame() {
-    const body = document.querySelector("body");
-    const div = document.createElement("div");
-    div.setAttribute("style", "background-color: white")
-    // div.setAttribute("style", "border: 1px solid black; background-color: pink");
-
-    let humanScore = 0;
-    let computerScore = 0;
 
     function playRound(humanChoice, computerChoice) {
-        if (humanChoice === computerChoice) {
+        if (gameState === true && humanChoice === computerChoice) {
             const draw = document.createElement("p");
             draw.setAttribute("style", "width: 200px; color: grey; font-weight: bold");
             draw.textContent = "It's a draw! Play Again";
             div.appendChild(draw);
             return;
-        } else if ((humanChoice === "rock" && computerChoice == "scissors") ||
+
+        } else if (gameState === true && (humanChoice === "rock" && computerChoice == "scissors") ||
             (humanChoice === "scissors" && computerChoice == "paper") ||
             (humanChoice === "paper" && computerChoice == "rock")) {
             const humanWins = document.createElement("p");
@@ -49,7 +51,10 @@ function playGame() {
             humanWins.textContent = `You won! ${humanChoice} beats ${computerChoice}.`;
             div.appendChild(humanWins);
             humanScore += 1;
-        } else if ((computerChoice === "rock" && humanChoice == "scissors") ||
+            humanScoreTracker.textContent = `${humanScore}`;
+            displayResults();
+
+        } else if (gameState === true && (computerChoice === "rock" && humanChoice == "scissors") ||
             (computerChoice === "scissors" && humanChoice == "paper") ||
             (computerChoice === "paper" && humanChoice == "rock")) {
             const computerWins = document.createElement("p");
@@ -57,6 +62,9 @@ function playGame() {
             computerWins.textContent = `Computer won! ${computerChoice} beats ${humanChoice}.`;
             div.appendChild(computerWins);
             computerScore += 1;
+            computerScoreTracker.textContent = `${computerScore}`;
+            displayResults();
+
         } else {
             const invalidChoice = document.createElement("p");
             invalidChoice.textContent = "Invalid choice! Try again";
@@ -75,22 +83,105 @@ function playGame() {
     const btn3 = document.querySelector("#btn3");
 
     btn1.addEventListener("click", () => {
-        processChoice("rock");
-        body.appendChild(div);
+        if (gameState == true) {
+            processChoice("rock");
+            body.appendChild(div);
+        } else {
+            return;
+        }
     });
 
     btn2.addEventListener("click", () => {
-        processChoice("paper");
-        body.appendChild(div);
+        if (gameState == true) {
+            processChoice("paper");
+            body.appendChild(div);
+        } else {
+            return;
+        }
+
     });
 
     btn3.addEventListener("click", () => {
-        processChoice("scissors");
-        body.appendChild(div);
+        if (gameState == true) {
+            processChoice("scissors");
+            body.appendChild(div);
+        } else {
+            return;
+        }
     });
 }
 
 
 
+
+
+
+function displayResults() {
+    const results = document.querySelector("#results");
+    const playAgain = document.createElement("button");
+    playAgain.textContent = "Play Again";
+
+    if (humanScore === 5) {
+        gameState = false;
+        results.setAttribute("style", "display: block; background-color: grey; color: green");
+        const p1 = document.createElement("p");
+        p1.textContent = "Congratulations! You won!";
+        results.appendChild(p1);
+
+        const p2 = document.createElement("p");
+        p2.textContent = `Your Score: ${humanScore}`;
+        results.appendChild(p2);
+
+        const p3 = document.createElement("p");
+        p3.textContent = `Computer Score: ${computerScore}`;
+        results.appendChild(p3);
+
+        results.appendChild(playAgain);
+        playAgain.addEventListener("click", () => {
+            results.style.display = "none";
+            while (results.firstChild) {
+                results.removeChild(results.firstChild);
+            }
+            while (div.firstChild) {
+                div.removeChild(div.firstChild);
+            }
+            humanScore = 0;
+            computerScore = 0;
+            humanScoreTracker.textContent = `${humanScore}`;
+            computerScoreTracker.textContent = `${computerScore}`;
+            gameState = true;
+        });
+    } else if (computerScore == 5) {
+        gameState = false;
+        results.setAttribute("style", "display: block; background-color: grey; color: green");
+        const p1 = document.createElement("p");
+        p1.textContent = "Computer won! You lose!";
+        results.appendChild(p1);
+
+        const p2 = document.createElement("p");
+        p2.textContent = `Your Score: ${humanScore}`;
+        results.appendChild(p2);
+
+        const p3 = document.createElement("p");
+        p3.textContent = `Computer Score: ${computerScore}`;
+        results.appendChild(p3);
+
+        results.appendChild(playAgain);
+        playAgain.addEventListener("click", () => {
+            results.style.display = "none";
+            while (results.firstChild) {
+                results.removeChild(results.firstChild);
+            }
+            while (div.firstChild) {
+                div.removeChild(div.firstChild);
+            }
+            humanScore = 0;
+            computerScore = 0;
+            humanScoreTracker.textContent = `${humanScore}`;
+            computerScoreTracker.textContent = `${computerScore}`;
+            gameState = true;
+        });
+    }
+}
 
 playGame();
